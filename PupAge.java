c7397edd3a7dc55409024daa51e9d16044fef334
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-29 23:05:35 +0600

    Add files via upload

---
+++ PupAge.java
@@ -0,0 +1,13 @@
+public class PupAge {
+    public static void main (String args[]){
+        PupAge pupAge =new PupAge();
+        pupAge.Age();
+
+    }
+
+    public void Age(){
+        int age=0;
+        age=age+7;
+        System.out.println(age);
+    }
+}

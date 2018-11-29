c9284bb4b0e2890c85e1a599b5ab5eee5bb07932
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-29 23:57:46 +0600

    Add files via upload

---
+++ Employee2.java
@@ -0,0 +1,9 @@
+public class Employee2 {
+    private static double salary;
+    public static final String DEPARTMENT = "Department";
+
+    public static void main(String[] args) {
+        salary = 1000;
+        System.out.println(DEPARTMENT + " salary "+ salary);
+    }
+}

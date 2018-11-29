89088df9217ad67d75d2fb270bd13c43652e39aa
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-30 00:18:18 +0600

    Add files via upload

---
+++ MiscOp.java
@@ -0,0 +1,10 @@
+public class MiscOp {
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=(a==1)?20:30;
+        System.out.println(b);
+        b =(a==10)?20:30;
+        System.out.println(b);
+    }
+}

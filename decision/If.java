ced0047c6897c06df61823cb7df0e5d971c5f214
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-30 11:31:58 +0600

    Add files via upload

---
+++ decision/If.java
@@ -0,0 +1,12 @@
+package decision;
+
+public class If {
+    public static void main(String[] args) {
+        int x = 20;
+        if (x==20){
+            System.out.println(x);
+        }else{
+            System.out.println("else");
+        }
+    }
+}
---
+++ decision/NestedIf.java
@@ -0,0 +1,13 @@
+package decision;
+
+public class NestedIf {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 10;
+        if (x==30){
+            if (y ==10) {
+                System.out.println(x+y);
+            }
+        }
+    }
+}

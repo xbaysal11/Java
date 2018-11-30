ad1730f01fe86f9c1f226cd176e50f8ad8ef6d47
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-30 10:26:28 +0600

    Add files via upload

---
+++ Loop/While.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class While {
+    public static void main(String[] args) {
+        int x=10;
+        while (x<20){
+            System.out.println(x);
+            x++;
+        }
+    }
+}

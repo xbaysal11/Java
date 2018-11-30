04c678927d7690cc18bc4427ae3faaab2bc4f404
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-30 10:29:04 +0600

    Add files via upload

---
+++ Loop/DoWhile.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int x=10;
+        do {
+            System.out.println(x);
+            x++;
+        }while (x<20);
+    }
+}

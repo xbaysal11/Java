39a77d5837cd5177b454a3af21ba51ea0de24434
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-30 10:51:40 +0600

    Add files via upload

---
+++ Loop/EnhancedFor.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class EnhancedFor {
+    public static void main(String[] args) {
+        int[] Arr1 = {10,20,30,40,50};
+        String[] Arr2 = {"a","b","c"};
+        for (int num:Arr1) {
+            System.out.println(num);
+        }
+        for (String str:Arr2) {
+            System.out.println(str);
+        }
+    }
+}
